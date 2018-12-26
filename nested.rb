3b25612bf22a2bc83b8bdff67f7ad5b044c527e9
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 11:39:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-  return programmer_hash[:grace_hopper]
+  
 
 end
 
@@ -39,9 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
-  return programmer_hash[:alan_kay][:known_for]
-end
+  
 
 def dennis_ritchies_language
 	programmer_hash = 
